f7990289f250761ea422261d6b374747f9cd7e72
Michael Cmar <michael.cmar@gmail.com>
2025-01-26 00:25:10 -0500

    updating config files

--- backend/settings.py
+++ backend/settings.py
@@ -1,1 +1,13 @@
-<Your Django settings.py code here>
+
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallbacksecretkey')
+
+ALLOWED_HOSTS = ['bookdtest.onrender.com']
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
