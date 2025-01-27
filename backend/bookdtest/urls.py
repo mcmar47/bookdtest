8f2f8ef3c58d30d05c63b6cde99f80485cf3b956
Michael Cmar <michael.cmar@gmail.com>
2025-01-27 01:10:09 -0500

    calendar changes

--- backend/bookdtest/urls.py
+++ backend/bookdtest/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import home  # ✅ Homepage
 from rest_framework.routers import DefaultRouter
-from venues.views import VenueViewSet
+from venues.views import VenueViewSet, EventViewSet
 from bookings.views import BookingRequestViewSet
 from messaging.views import MessageViewSet
 from users.views import UserViewSet
@@ -13,6 +13,8 @@ router.register(r'venues', VenueViewSet, basename='venues')
 router.register(r'bookings', BookingRequestViewSet, basename='bookings')
 router.register(r'messages', MessageViewSet, basename='messages')
 router.register(r'users', UserViewSet, basename='users')
+router.register(r'events', EventViewSet, basename='events')  # <-- Add this line
+
 
 urlpatterns = [
     path('', home, name='home'),  # ✅ Homepage view
